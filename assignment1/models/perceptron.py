5ab5a97fa971f2b8e1cbcda9b4feb29c40fdb12f
Alice Huang <aliceh4@illinois.edu>
2022-02-07 15:48:30 -0600

    Initial commit

---
+++ assignment1/models/perceptron.py
@@ -0,0 +1,66 @@
+"""Perceptron model."""
+
+import numpy as np
+
+
+class Perceptron:
+    def __init__(self, n_class: int, lr: float, epochs: int):
+        """Initialize a new classifier.
+
+        Parameters:
+            n_class: the number of classes
+            lr: the learning rate
+            epochs: the number of epochs to train for
+        """
+        # initialize weights to be all zeros, with 10 rows (for each class)
+        # and X_train[1] number of columns
+        self.w =  None # TODO: change this
+        self.lr = lr
+        self.epochs = epochs
+        self.n_class = n_class
+
+    def train(self, X_train: np.ndarray, y_train: np.ndarray):
+        """Train the classifier.
+
+        Use the perceptron update rule as introduced in the Lecture.
+
+        Parameters:
+            X_train: a number array of shape (N, D) containing training data;
+                N examples with D dimensions
+            y_train: a numpy array of shape (N,) containing training labels
+        """
+        np.random.seed(0)
+        self.w = np.random.rand(X_train.shape[1], self.n_class)
+        for _ in range(0, self.epochs):
+            # iterate through each training sample
+            for xi, yi in zip(X_train, y_train):
+                wx = np.dot(xi, self.w) # (1 x D) * (D x 10)
+                weight_actual = wx[yi]
+
+                # loop through each class
+                for c in range(0, self.n_class):
+                    # if our incorrect class argument is higher than the actual class argument 
+                    if wx[c] > weight_actual:
+                        self.w[:,yi] = self.w[:,yi] + self.lr * xi
+                        self.w[:,c] = self.w[:,c] - self.lr * xi
+        
+
+    def predict(self, X_test: np.ndarray) -> np.ndarray:
+        """Use the trained weights to predict labels for test data points.
+
+        Parameters:
+            X_test: a numpy array of shape (N, D) containing testing data;
+                N examples with D dimensions
+
+        Returns:
+            predicted labels for the data in X_test; a 1-dimensional array of
+                length N, where each element is an integer giving the predicted
+                class.
+        """
+        N = X_test.shape[0]
+        predicted = np.zeros(N)
+        for i in range(0, N): 
+            xi = X_test[i]
+            wx = np.dot(xi.T, self.w) # (1, D) * (D x 10)
+            predicted[i] = np.argmax(wx)
+        return predicted.astype(int)
---
+++ assignment1/models/svm.py
@@ -0,0 +1,93 @@
+"""Support Vector Machine (SVM) model."""
+
+import numpy as np
+
+
+class SVM:
+    def __init__(self, n_class: int, lr: float, epochs: int, reg_const: float):
+        """Initialize a new classifier.
+
+        Parameters:
+            n_class: the number of classes
+            lr: the learning rate
+            epochs: the number of epochs to train for
+            reg_const: the regularization constant
+        """
+        self.w = None  # TODO: change this
+        self.lr = lr
+        self.epochs = epochs
+        self.reg_const = reg_const
+        self.n_class = n_class
+
+    def calc_gradient(self, X_train: np.ndarray, y_train: np.ndarray) -> np.ndarray:
+        """Calculate gradient of the svm hinge loss.
+
+        Inputs have dimension D, there are C classes, and we operate on
+        mini-batches of N examples.
+
+        Parameters:
+            X_train: a numpy array of shape (N, D) containing a mini-batch
+                of data
+            y_train: a numpy array of shape (N,) containing training labels;
+                y[i] = c means that X[i] has label c, where 0 <= c < C
+
+        Returns:
+            the gradient with respect to weights w; an array of the same shape
+                as w
+        """
+        # weight: D x class array
+        n = X_train.shape[0]
+        gradient = self.reg_const / n * self.w # set gradient equal to weight*constant
+        for xi, yi in zip(X_train, y_train):
+            wx = np.dot(xi, self.w) # (1, D) * (D * n_class) = 1 x n_class array
+            sum = 0
+            for c in range(0, self.n_class):
+                if c == yi:
+                    continue
+
+                # now we know c != yi
+                if wx[yi] - wx[c] < 1:
+                    gradient[:, c] += xi
+
+                    # calculate for w_yi result
+                    sum += xi
+
+            gradient[:, yi] -= sum
+        return gradient
+
+    def train(self, X_train: np.ndarray, y_train: np.ndarray):
+        """Train the classifier.
+
+        Hint: operate on mini-batches of data for SGD.
+
+        Parameters:
+            X_train: a numpy array of shape (N, D) containing training data;
+                N examples with D dimensions
+            y_train: a numpy array of shape (N,) containing training labels
+        """
+        np.random.seed(0)
+        self.w = np.random.rand(X_train.shape[1], self.n_class)
+        for _ in range(0, self.epochs):
+            gradient = self.calc_gradient(X_train, y_train)
+            self.w += -self.lr * gradient
+
+
+    def predict(self, X_test: np.ndarray) -> np.ndarray:
+        """Use the trained weights to predict labels for test data points.
+
+        Parameters:
+            X_test: a numpy array of shape (N, D) containing testing data;
+                N examples with D dimensions
+
+        Returns:
+            predicted labels for the data in X_test; a 1-dimensional array of
+                length N, where each element is an integer giving the predicted
+                class.
+        """
+        N = X_test.shape[0]
+        predicted = np.zeros(N)
+        for i in range(0, N): 
+            xi = X_test[i]
+            wx = np.dot(xi.T, self.w) # (1, D) * (D x 10)
+            predicted[i] = np.argmax(wx)
+        return predicted.astype(int)
